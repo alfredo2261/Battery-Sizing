b1bf814a4886c490771ea22aa8e3526a91e79a50
alfredo2261 <alfredo.rodriguez2017@gmail.com>
2025-11-03 11:59:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -126,9 +126,9 @@ if load is not None:
     
     st.pyplot(fig)
 
-    print(output_kw)
-    print(load.values)
-    print(np.subtract(existing_load_new, output_kw[start:end]))
+    st.write(output_kw)
+    st.write(load.values)
+    st.write(np.subtract(existing_load_new, output_kw[start:end]))
     
     data = {
         'Battery':output_kw,
