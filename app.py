76b57cd0689838d53b67d9717c6cc6f04676e115
alfredo2261 <alfredo.rodriguez2017@gmail.com>
2026-01-16 15:03:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ if load is not None:
     timestep = st.number_input("Enter the time granularity of the uploaded load data (in minutes): ", value = 0)
     threshold = st.number_input("Enter the transformer normal thermal rating (in kW): ", value = 0)
     year = st.number_input("Enter the number of years the traditional solution will be deferred (from 0-15): ", value = 0)
-    growth_rate = st.number_input("Enter the annual load growth rate (from 0-1): ", min_value = 0.0, max_value = 1.0, step=0.001, format="%.3f")
+    growth_rate = st.number_input("Enter the annual load growth rate (from 0-1): ", min_value = 0.0, max_value = 1.0, step=0.0001, format="%.4f")
     degradation = st.number_input("Enter the annual degradation rate (from 0-1): ", value = 0.000, step=0.001, format="%0.3f")
     dod = st.number_input("Enter the depth of discharge of the battery (from 0-1): ", value = 0.000, step=0.001, format="%0.3f")
     rte = st.number_input("Enter the round-trip efficiency of the battery (from 0-1): ", value = 0.000, step=0.001, format="%0.3f")
